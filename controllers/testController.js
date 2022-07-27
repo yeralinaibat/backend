f488d07f932e709ce6011db3065ce7082b6ef354
yeralinaibat <yeralin4944@gmail.com>
2022-07-27 17:08:47 +0600

    comit

--- controllers/testController.js
+++ controllers/testController.js
@@ -40,7 +40,7 @@ class testController {
   async getCategories(req, res) {
     try {
       const tests = await Test.find();
-      const categories = new Set(tests.map((test) => test.category));
+      const categories = new Set(tests.map((test) => test.category.languages));
       let unique = [];
       for (let category of categories.values()) {
         unique.push({
